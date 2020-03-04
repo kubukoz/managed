d1a33b6fb97eba153033f1f92a6c0bc28fe73dc5
Jakub Kozłowski <kubukoz@gmail.com>
2020-03-04 01:49:47 +0100

    Simplify encoding, rename Use aliases

--- src/main/scala/com/kubukoz/managed/Managed.scala
+++ src/main/scala/com/kubukoz/managed/Managed.scala
@@ -9,42 +9,40 @@ import cats.data.Kleisli
 /**
   * A continuation-based alternative to Resource.
   */
-trait Managed[F[_], A] {
-  def use[B](f: A => F[B]): F[B]
+final case class Managed[F[_], A](private val useManaged: Managed.Use0[F, A]) {
+  def use[B](f: A => F[B]): F[B] = useManaged[B](f)
 }
 
 object Managed {
-  type Use2[F[_], A, b] = (A => F[b]) => F[b]
-  type Use[F[_], A] = ∀[Use2[F, A, *]]
+  // Usage of a resource. For values, use like ∀[Use[F, A, *]](cb => cb(resource)).
+  // For types, better go with Use0[F, A]
+  type Use[F[_], A, b] = (A => F[b]) => F[b]
+  type Use0[F[_], A] = ∀[Use[F, A, *]]
 
-  def managed[F[_], A](use: Use[F, A]): Managed[F, A] = Instance(use)
+  def managed[F[_], A](use: Use0[F, A]): Managed[F, A] = new Managed(use)
 
-  def liftF[F[_]: FlatMap, A](fa: F[A]): Managed[F, A] = managed(∀[Use2[F, A, *]](fa.flatMap(_)))
+  def liftF[F[_]: FlatMap, A](fa: F[A]): Managed[F, A] = managed(∀[Use[F, A, *]](fa.flatMap(_)))
 
   def fromResource[F[_]: Bracket[*[_], Throwable], A](resource: Resource[F, A]): Managed[F, A] =
-    managed(∀[Use2[F, A, *]](resource.use(_)))
+    managed(∀[Use[F, A, *]](resource.use(_)))
 
   implicit def managedMonad[F[_]: Monad]: Monad[Managed[F, *]] =
     new StackSafeMonad[Managed[F, *]] {
 
       def flatMap[A, B](fa: Managed[F, A])(f: A => Managed[F, B]): Managed[F, B] =
         managed {
-          ∀[Use2[F, B, *]](use => fa.use(f(_).use(use)))
+          ∀[Use[F, B, *]](use => fa.use(f(_).use(use)))
         }
 
-      def pure[A](x: A): Managed[F, A] = Managed.managed(∀[Use2[F, A, *]](_(x)))
+      def pure[A](x: A): Managed[F, A] = Managed.managed(∀[Use[F, A, *]](_(x)))
     }
-
-  final case class Instance[F[_], A](useManaged: Use[F, A]) extends Managed[F, A] {
-    def use[B](f: A => F[B]): F[B] = useManaged[B](f)
-  }
 }
 
 object KleisliManagedExamples extends IOApp {
 
   def local[F[_], A](modValue: A => A): Managed[Kleisli[F, A, *], Unit] =
     Managed.managed {
-      ∀[Managed.Use2[Kleisli[F, A, *], Unit, *]] { use =>
+      ∀[Managed.Use[Kleisli[F, A, *], Unit, *]] { use =>
         Kleisli { original =>
           use(()).run(modValue(original))
         }
